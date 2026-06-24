e318dea25ecda540c806af6e0ed9e2f1d2ca6e3c
sabrikanli <sabrikanli@proton.me>
2026-06-24 04:14:13 +0300

    updated

--- backend/calculator.py
+++ backend/calculator.py
@@ -1,8 +1,29 @@
 import json
 from pathlib import Path
 
-def calculate_point(year, obp, tr, sos, mat, fen, mat2, fiz2, kimya2, biyo2):
+def calculate_score(year, obp, tr, sos, mat, fen, mat2, fiz2, kimya2, biyo2):
 
+    if obp < 0:
+        return "Ortaöğretim Başarı Puanı 50'den küçük olamaz."
+    if obp > 100:
+        return "Ortaöğretim Başarı Puanı 100'den büyük olamaz."
+    if tr > 40:
+        return "Türkçe testinde 40 soru bulunmaktadır."
+    if sos > 20:
+        return "Sosyal Bilimler testinde 20 soru bulunmaktadır."
+    if mat > 40:
+        return "Matematik testinde 40 soru bulunmaktadır."
+    if fen > 20:
+        return "Fen Bilimleri testinde 20 soru bulunmaktadır."
+    if mat2 > 40:
+        return "Matematik 2 testinde 40 soru bulunmaktadır."
+    if fiz2 > 14:
+        return "Fizik 2 testinde 14 soru bulunmaktadır."
+    if kimya2 > 13:
+        return "Kimya 2 testinde 13 soru bulunmaktadır."
+    if biyo2 > 13:
+        return "Biyoloji 2 testinde 13 soru bulunmaktadır."
+    
     current_dir = Path(__file__).resolve().parent
 
     json_path = current_dir.parent / "data" / "weights.json"
@@ -12,7 +33,7 @@ def calculate_point(year, obp, tr, sos, mat, fen, mat2, fiz2, kimya2, biyo2):
         current_year_weights = weights[str(year)]["say"]
 
     return (
-        current_year_weights["taban"]
+        int(current_year_weights["taban"]
         + current_year_weights["tr"] * tr
         + current_year_weights["sos"] * sos
         + current_year_weights["mat"] * mat
@@ -21,6 +42,6 @@ def calculate_point(year, obp, tr, sos, mat, fen, mat2, fiz2, kimya2, biyo2):
         + current_year_weights["fiz2"] * fiz2
         + current_year_weights["kimya2"] * kimya2
         + current_year_weights["biyo2"] * biyo2
-        + obp * 0.6
+        + obp * 0.6)
     )
 
--- backend/rank.py
+++ backend/rank.py
@@ -1,8 +1,8 @@
 import json
 from pathlib import Path
-from calculator import calculate_point
+from calculator import calculate_score
 
-def hesapla_siralama_spline(score, year):
+def calculate_rank(score, year):
     
     current_dir = Path(__file__).resolve().parent
     json_path = current_dir.parent / "data" / "spline.json"
@@ -11,22 +11,23 @@ def hesapla_siralama_spline(score, year):
         spline = json.load(f)
         spline_data = spline[str(year)]
 
-    if score >= 560:
+    if int(score) >= 560:
         return 1
-    if score <= 100:
+    if int(score) <= 100:
         return 3500000
 
-    interval = next((s for s in spline_data if s["x_min"] <= score <= s["x_max"]), None)
+    interval = next((s for s in spline_data if s["x_min"] <= int(score) <= s["x_max"]), None)
 
     if not interval:
         return -1 
-    dx = score - interval["x_min"]
+    dx = int(score) - interval["x_min"]
     
-    siralama = (interval["a"] * (dx ** 3)) + \
+    rank = (interval["a"] * (dx ** 3)) + \
                (interval["b"] * (dx ** 2)) + \
                (interval["c"] * dx) + \
                interval["d"]
-    return max(1, round(siralama))
+    return max(1, round(rank))
 
-test_puani = calculate_point(2025, 100, 30, 10, 30, 6, 28, 10, 10, 0)
-print(f"{test_puani} Puan Sıralaması:", hesapla_siralama_spline(test_puani, 2025))
+test_score = calculate_score(2025, 100, 41, 10, 30, 6, 28, 10, 10, 0)
+
+print(f"{test_score} Puan Sıralaması:", calculate_rank(test_score, 2025))
